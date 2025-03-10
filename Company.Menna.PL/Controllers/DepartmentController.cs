4813f2dc9b4b61cf83d2ff99beb81edd8ebe5595
Admin <Admin@DESKTOP-1R0450K>
2025-03-10 02:16:39 -0700

    ActionCreate

--- Company.Menna.PL/Controllers/DepartmentController.cs
+++ Company.Menna.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.Menna.BLL.Interfaces;
 using Company.Menna.BLL.Repositories;
+using Company.Menna.DAL.Models;
+using Company.Menna.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Menna.PL.Controllers
@@ -20,10 +22,35 @@ namespace Company.Menna.PL.Controllers
 
             var department = _departmentRepositories.GetAll();
 
-
-
-
             return View(department);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if(ModelState.IsValid)
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt,
+                };
+               var count = _departmentRepositories.Add(department);
+                if(count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View();
+        }
+
     }
 }
