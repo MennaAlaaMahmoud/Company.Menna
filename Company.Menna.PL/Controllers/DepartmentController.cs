2b0938c3c7e8f069506524372eea55c03686390f
Admin <Admin@DESKTOP-1R0450K>
2025-03-12 23:44:09 -0700

    Action Update

--- Company.Menna.PL/Controllers/DepartmentController.cs
+++ Company.Menna.PL/Controllers/DepartmentController.cs
@@ -64,6 +64,65 @@ namespace Company.Menna.PL.Controllers
 
         }
 
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id is null) return BadRequest("Invalid Id");// 400
+
+            var department = _departmentRepositories.Get(id.Value);
+            if (department is null) return NotFound(new { StatusCode = 400, message = $"Department With Id : {id} is not found" });
+
+            return View(department);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id, Department department)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (id != department.Id) return BadRequest();
+                {
+                    var Count = _departmentRepositories.Update(department);
+                    if (Count > 0)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+
+
+            return View(department);
+        }
+
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Edit([FromRoute] int id, UpdateDepartmentDto model)
+        //{
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        var department = new Department()
+        //        {
+        //            Id = id,
+        //            Name = model.Name,
+        //            Code = model.Code,
+        //            CreateAt = model.CreateAt,
+
+        //        };
+        //            var Count = _departmentRepositories.Update(department);
+        //            if (Count > 0)
+        //            {
+        //                return RedirectToAction(nameof(Index));
+        //            }
+
+        //    }
+
+
+        //    return View(model);
+        //}
 
 
     }
