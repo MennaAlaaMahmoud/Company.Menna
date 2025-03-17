04b75bc4b7c9dad7415bfd7980aa4a85ef2b8aa6
Admin <Admin@DESKTOP-1R0450K>
2025-03-17 02:44:46 -0700

    Implementations Of BLL For Employee Service Before Generic

---
+++ Company.Menna.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Menna.DAL.Models;
+
+namespace Company.Menna.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee? Get(int id);
+        int Add (Employee model);
+        int Update (Employee model);
+        int Delete (Employee model);
+    }
+}
---
+++ Company.Menna.BLL/Repositories/EmployeeRepositories.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Menna.BLL.Interfaces;
+using Company.Menna.DAL.Data.Context;
+using Company.Menna.DAL.Models;
+
+namespace Company.Menna.BLL.Repositories
+{
+    public class EmployeeRepositories : IEmployeeRepository
+    {
+        private readonly CompanyDbContext _context;
+
+        public EmployeeRepositories(CompanyDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+        public Employee? Get(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+
+        public int Add(Employee model)
+        {
+            _context.Employees.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Employee model)
+        {
+            _context.Employees.Update(model);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Employee model)
+        {
+            _context.Employees.Remove(model);
+            return _context.SaveChanges();
+        }
+
+       
+      
+     
+    }
+}
